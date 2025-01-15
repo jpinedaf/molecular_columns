472dddb2d68f5060145e472ca605d4069e638915
Jaime Pineda <jaime.e.pineda@gmail.com>
2025-01-15 18:02:40 +0100

    updated NH2D -new tests -added unit control

---
+++ src/molecular_columns/test/test_col_nh2d.py
@@ -0,0 +1,37 @@
+import astropy.units as u
+
+import molecular_columns.col_nh2d as col_nh2d
+import pytest
+from astropy.units.errors import UnitsError
+import numpy as np
+
+
+def test_col_nh2d_Q_op_NH2D() -> None:
+    value = col_nh2d.Q_o_NH2D(Tex=5*u.K)
+    np.testing.assert_almost_equal(value, 9.69344271)
+    #
+    value = col_nh2d.Q_p_NH2D(Tex=5*u.K).value
+    assert pytest.approx(value, abs=0.001) == 3.5770964
+    #
+    # test with temperature arrays
+    value = col_nh2d.Q_o_NH2D(Tex=[5., 5., 5.]*u.K)
+    np.testing.assert_almost_equal(value, 9.69344271)
+    #
+    value = col_nh2d.Q_p_NH2D(Tex=[5., 5., 5.]*u.K)
+    np.testing.assert_almost_equal(value, 3.5770964)
+
+
+def test_col_nh2d_invalid_units() -> None:
+    with pytest.raises(UnitsError):
+        col_nh2d.Q_p_NH2D(Tex=5*u.m)
+    with pytest.raises(UnitsError):
+        col_nh2d.Q_o_NH2D(Tex=5*u.m)
+
+
+def test_col_nh2d_values() -> None:
+    result = col_nh2d.o_NH2D_thick(Tex=5*u.K, sigma_v=0.2*u.km/u.s, tau=2.0)
+    # np.testing.assert_almost_equal(result.to(u.cm**-2).value, 1.32950178e+14)
+    assert pytest.approx(result.to(u.cm**-2).value) == 1.32950178e+14
+
+    result = col_nh2d.p_NH2D_thick(Tex=5*u.K, sigma_v=0.2*u.km/u.s, tau=2.0)
+    assert pytest.approx(result.to(u.cm**-2).value) == 1.12581955e+14
