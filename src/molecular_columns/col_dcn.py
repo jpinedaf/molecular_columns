570d40d6d68bd9b81f850b16abade4d6e6c37aff
Jaime Pineda <jaime.e.pineda@gmail.com>
2025-01-17 12:29:20 +0100

    update DCN -create docstrings -create tests

--- src/molecular_columns/col_dcn.py
+++ src/molecular_columns/col_dcn.py
@@ -1,40 +1,55 @@
 import numpy as np
+from importlib.resources import files
 import astropy.units as u
 from astropy.constants import c, k_B, h
-
-from importlib.resources import files
 
 from .common_functions import J_nu
 
 # transition properties obtained from splatalogue:
 # dcn.dat (downloaded 2023 nov 7)
-file_mol = files('molecular_columns').joinpath('dcn.dat')
+file_mol = files("molecular_columns").joinpath("dcn.dat")
 gu_list = np.loadtxt(file_mol, usecols=4)
-
 E_u_list = np.loadtxt(file_mol, usecols=3) * u.K
-
 full_index = np.arange(np.size(E_u_list))
-
 freq_list = np.loadtxt(file_mol, usecols=0) * u.GHz
-
-Aij_list = 10.0**(np.loadtxt(file_mol, usecols=2)) / u.s
-
-T_bg = 2.73 * u.K
+Aij_list = 10.0 ** (np.loadtxt(file_mol, usecols=2)) / u.s
 
 
-def Q_DCN_i(index, Tex=5*u.K):
+@u.quantity_input
+def Q_DCN_i(index: int, Tex: u.K = 5 * u.K) -> float:
     """
     The function returns the individual elements of the partition function:
     the occupancy of each level dependent on degeneracy and energy level
     for a given excitation temperature.
+
+    Parameters
+    ----------
+    index : int
+        The index of the energy level.
+    Tex : u.K
+        The excitation temperature.
+    Returns
+    -------
+    float
+        The occupancy of the level.
     """
     return gu_list[index] * np.exp(-E_u_list[index] / Tex)
 
 
-def Q_DCN(Tex=5*u.K):
+@u.quantity_input
+def Q_DCN(Tex: u.K = 5 * u.K) -> float:
     """
-    It returns the particion function for DCN with an excitation
+    It returns the partition function for DCN with an excitation
     temperature.
+
+    Parameters
+    ----------
+    Tex : u.K
+        The excitation temperature.
+    Returns
+    -------
+    Q_DCN_all : float
+        The partition function.
     """
     if Tex.size == 1:
         return np.sum(Q_DCN_i(full_index, Tex=Tex))
@@ -45,21 +60,49 @@ def Q_DCN(Tex=5*u.K):
         return Q_DCN_all
 
 
-def DCN_thin(J_up=1, Tex=5*u.K, TdV=1.0*u.K*u.km/u.s):
+@u.quantity_input
+def DCN_thin(
+    J_up: int = 1,
+    Tex: u.K = 5 * u.K,
+    TdV: u.K * u.km / u.s = 1.0 * u.K * u.km / u.s,
+    T_bg: u.K = 2.73 * u.K,
+) -> u.cm**-2:
     """
     Total column density determination from the DCN J_up -> J_up-1 transition.
     The A_ul, frequency and Einstein coefficient are obtained from CDMS.
+
+    Parameters
+    ----------
+    J_up : int
+        The upper level of the transition.
+    Tex : u.K
+        The excitation temperature.
+    TdV : u.K * u.km / u.s
+        The integrated intensity of the transition.
+    T_bg : u.K
+        The background temperature.
+
+    Returns
+    -------
+    Ncol : u.cm**-2
+        The column density.
     """
     if J_up < np.size(Aij_list):
         freq = freq_list[J_up - 1]
         A_ul = Aij_list[J_up - 1]
     else:
-        print('J_up is not available')
-        return np.nan
+        print("J_up is not available")
+        return np.nan * u.cm**-2
     Jex = J_nu(Tex=Tex, freq=freq)
     Jbg = J_nu(Tex=T_bg, freq=freq)
 
-    Ncol = (8*np.pi*freq**3/c**3) * Q_DCN(Tex=Tex) \
-         / A_ul / Q_DCN_i(J_up, Tex=Tex) \
-         / (np.exp(h*freq/k_B/Tex) - 1) * TdV / (Jex - Jbg)
-    return Ncol.to(u.cm**-2)
+    Ncol = (
+        (8 * np.pi * freq**3 / c**3)
+        * Q_DCN(Tex=Tex)
+        / A_ul
+        / Q_DCN_i(J_up, Tex=Tex)
+        / (np.exp(h * freq / k_B / Tex) - 1)
+        * TdV
+        / (Jex - Jbg)
+    )
+    return Ncol
---
+++ src/molecular_columns/test/test_col_dcn.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pytest
+import astropy.units as u
+import molecular_columns.col_dcn as col_dcn
+from astropy.units.errors import UnitsError
+
+
+def test_col_dcn_Q_DCN():
+    value = col_dcn.Q_DCN(Tex=5 * u.K)
+    # Replace with the expected value
+    assert pytest.approx(value.value, abs=0.001) == 6.70685842
+
+    value = col_dcn.Q_DCN(Tex=[5., 5., 5.] * u.K)
+    np.testing.assert_almost_equal(value, [6.70685842, 6.70685842, 6.70685842])
+
+
+def test_col_dcn_invalid_units():
+    with pytest.raises(UnitsError):
+        col_dcn.Q_DCN(Tex=5 * u.m)
+
+
+def test_col_dcn_thin():
+    result = col_dcn.DCN_thin(J_up=63, Tex=5 * u.K,
+                              TdV=1000.0 * u.K * u.km / u.s)
+    assert np.isnan(result.value)
+    result = col_dcn.DCN_thin(J_up=1, Tex=5 * u.K, TdV=1.0 * u.K * u.km / u.s)
+    assert pytest.approx(result.to(u.cm**-2).value,
+                         rel=0.001) == 4.56431689e+12
