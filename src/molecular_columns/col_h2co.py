67fbbaeebc41e44bed137d71ae446b2475fe2b2b
Jaime Pineda <jaime.e.pineda@gmail.com>
2025-07-07 12:00:54 +0200

    Refactor col_h2co.py for improved readability and consistency; update function signatures and formatting.

--- src/molecular_columns/col_h2co.py
+++ src/molecular_columns/col_h2co.py
@@ -1,74 +1,77 @@
 import numpy as np
-import requests
+from numpy.typing import NDArray
 import astropy.units as u
-from astropy.constants import c, k_B, h
+from astropy.constants import c, k_B, h  # type: ignore
 from .common_functions import J_nu
-try:
-    from importlib.resources import files
-except ImportError:
-    from importlib_resources import files
+from importlib.resources import files
+
 
 # g_u, E_u, and A_ul values obtained from LAMBDA database
-
 def extract_from_lambda(file_name):
     # Read the content of the file
     file_mol = files("molecular_columns").joinpath(file_name)
-    f = open(file_mol, 'r')
+    f = open(str(file_mol), "r")
     # Read the content line by line
     lines = f.readlines()
     f.close()
-    
+
     # Make an empty dictionary for J_Kp_Ko, E_u (cm-1), and g_u values
-    level_dict={'J_Kp_Ko':[], 'E_u':[], 'g_u':[]}
+    level_dict = {"J_Kp_Ko": [], "E_u": [], "g_u": []}
     # Make an empty dictionary for frequency (GHz), A_ul, and E_u (K) values
-    trans_dict={'freq':[], 'A_ul':[], 'E_u':[], 'upper_level_no':[]}
+    trans_dict = {"freq": [], "A_ul": [], "E_u": [], "upper_level_no": []}
 
     # Extract WEIGHT values
     for line in lines:
-        if line=="!NUMBER OF COLL PARTNERS":
+        if line == "!NUMBER OF COLL PARTNERS":
             break
         parts = line.split()
-        if line[0]=="!":
+        if line[0] == "!":
             pass
         else:
-            if len(parts) ==4: # LEVEL + ENERGIES(cm^-1) + WEIGHT + J_Kp_Ko
+            if len(parts) == 4:  # LEVEL + ENERGIES(cm^-1) + WEIGHT + J_Kp_Ko
                 try:
-                    energy = float(parts[1]) 
-                    level_dict['E_u'].append(energy) 
-                    weight = float(parts[2])  
-                    level_dict['g_u'].append(weight)
-                    level_dict['J_Kp_Ko'].append(parts[3])
+                    energy = float(parts[1])
+                    level_dict["E_u"].append(energy)
+                    weight = float(parts[2])
+                    level_dict["g_u"].append(weight)
+                    level_dict["J_Kp_Ko"].append(parts[3])
                 except ValueError:
                     pass  # Skip lines that don't contain valid numbers
-            if len(parts) ==6: #TRANS + UP + LOW + EINSTEINA(s^-1) + FREQ(GHz) + E_u(K)
+            if (
+                len(parts) == 6
+            ):  # TRANS + UP + LOW + EINSTEINA(s^-1) + FREQ(GHz) + E_u(K)
                 try:
                     upper_level_no = int(parts[1])
-                    trans_dict['upper_level_no'].append(upper_level_no)
+                    trans_dict["upper_level_no"].append(upper_level_no)
                     einsteinA = float(parts[3])
-                    trans_dict['A_ul'].append(einsteinA)
+                    trans_dict["A_ul"].append(einsteinA)
                     freq = float(parts[4])
-                    trans_dict['freq'].append(freq)
+                    trans_dict["freq"].append(freq)
                     upper_level_energy = float(parts[5])
-                    trans_dict['E_u'].append(upper_level_energy)
-                    
+                    trans_dict["E_u"].append(upper_level_energy)
+
                 except ValueError:
                     pass
-   
+
     return level_dict, trans_dict
+
 
 p_level_dict, p_trans_dict = extract_from_lambda("ph2co-h2.dat")
 o_level_dict, o_trans_dict = extract_from_lambda("oh2co-h2.dat")
-gu_p_list = np.array(p_level_dict['g_u'])
-E_u_p_list = (np.array(p_level_dict['E_u'])* (h * c / k_B) / u.cm).to(u.K)
-gu_o_list = np.array(o_level_dict['g_u'])
-E_u_o_list = (np.array(o_level_dict['E_u'])* (h * c / k_B) / u.cm).to(u.K)
+gu_p_list = np.array(p_level_dict["g_u"])
+E_u_p_list = (np.array(p_level_dict["E_u"]) * (h * c / k_B) / u.cm).to(u.K)  # type: ignore
+gu_o_list = np.array(o_level_dict["g_u"])
+E_u_o_list = (np.array(o_level_dict["E_u"]) * (h * c / k_B) / u.cm).to(u.K)  # type: ignore
 
 p_full_index = np.arange(np.size(E_u_p_list))
 o_full_index = np.arange(np.size(E_u_o_list))
 
 
 @u.quantity_input
-def Q_p_H2CO_i(index: int, Tex: u.K = 5 * u.K) -> float:
+def Q_p_H2CO_i(
+    index: int | NDArray[np.int_],
+    Tex: u.K = 5 * u.K,  # type: ignore
+) -> float | NDArray[np.float64]:
     """
     The function returns the individual elements of the partition function:
     the occupancy of each level dependent on degeneracy and energy level
@@ -89,7 +92,9 @@ def Q_p_H2CO_i(index: int, Tex: u.K = 5 * u.K) -> float:
 
 
 @u.quantity_input
-def Q_p_H2CO(Tex: u.K = 5 * u.K) -> float:
+def Q_p_H2CO(
+    Tex: u.K = 5 * u.K,  # type: ignore
+) -> float | NDArray[np.float64]:
     """
     It returns the partition function for para-H2CO with an excitation
     temperature.
@@ -113,7 +118,10 @@ def Q_p_H2CO(Tex: u.K = 5 * u.K) -> float:
 
 
 @u.quantity_input
-def Q_o_H2CO_i(index: int, Tex: u.K = 5 * u.K) -> float:
+def Q_o_H2CO_i(
+    index: int | NDArray[np.int_],
+    Tex: u.K = 5 * u.K,  # type: ignore
+) -> float | NDArray[np.float64]:
     """
     The function returns the individual elements of the partition function:
     the occupancy of each level dependent on degeneracy and energy level
@@ -134,7 +142,9 @@ def Q_o_H2CO_i(index: int, Tex: u.K = 5 * u.K) -> float:
 
 
 @u.quantity_input
-def Q_o_H2CO(Tex: u.K = 5 * u.K) -> float:
+def Q_o_H2CO(
+    Tex: u.K = 5 * u.K,  # type: ignore
+) -> float | NDArray[np.float64]:
     """
     It returns the partition function for ortho-H2CO with an excitation
     temperature.
@@ -159,8 +169,11 @@ def Q_o_H2CO(Tex: u.K = 5 * u.K) -> float:
 
 @u.quantity_input
 def p_H2CO_thin(
-    freq: float = 218.222192* u.GHz, Tex: u.K = 5*u.K, TdV: u.K*u.km/u.s = 1.0*u.K*u.km/u.s, T_bg: u.K= 2.73*u.K
-) -> u.cm**-2:
+    freq: u.GHz = 218.222192 * u.GHz,  # type: ignore
+    Tex: u.K = 5 * u.K,  # type: ignore
+    TdV: u.K * u.km / u.s = 1.0 * u.K * u.km / u.s,  # type: ignore
+    T_bg: u.K = 2.73 * u.K,  # type: ignore
+) -> u.cm**-2:  # type: ignore
     """
     Column density determination for the para-H2CO (1_{11}-1{01}) transition.
     The frequency and Einstein coefficient are obtained from LAMBDA database.
@@ -175,16 +188,16 @@ def p_H2CO_thin(
         The integrated intensity.
     T_bg : u.K
         The background temperature.
-    
+
     Returns
     -------
     Ncol : u.cm**-2
         The column density.
     """
-    trans_index = np.where(p_trans_dict['freq'] == freq.value)[0][0]
-    A_ul = p_trans_dict['A_ul'][trans_index] / u.s
-    upper_level_no = p_trans_dict['upper_level_no'][trans_index] #new
-    upper_level_index = upper_level_no-1 #new
+    trans_index = np.where(p_trans_dict["freq"] == freq.value)[0][0]
+    A_ul = p_trans_dict["A_ul"][trans_index] / u.s  # type: ignore
+    upper_level_no = p_trans_dict["upper_level_no"][trans_index]  # new
+    upper_level_index = upper_level_no - 1  # new
     Jex = J_nu(Tex=Tex, freq=freq)
     Jbg = J_nu(Tex=T_bg, freq=freq)
     print(A_ul, upper_level_no, upper_level_index)
@@ -192,17 +205,21 @@ def p_H2CO_thin(
         (8 * np.pi * freq**3 / c**3)
         * Q_p_H2CO(Tex=Tex)
         / A_ul
-        / Q_p_H2CO_i(upper_level_index, Tex=Tex) #modified
+        / Q_p_H2CO_i(upper_level_index, Tex=Tex)  # modified
         / (np.exp(h * freq / k_B / Tex) - 1)
-        * TdV/ (Jex - Jbg)
+        * TdV
+        / (Jex - Jbg)
     )
     return Ncol
 
 
 @u.quantity_input
 def o_H2CO_thin(
-    freq: float = 218.222192* u.GHz, Tex: u.K = 5*u.K, TdV: u.K*u.km/u.s = 1.0*u.K*u.km/u.s, T_bg: u.K= 2.73*u.K
-) -> u.cm**-2:
+    freq: u.GHz = 218.222192 * u.GHz,  # type: ignore
+    Tex: u.K = 5 * u.K,  # type: ignore
+    TdV: u.K * u.km / u.s = 1.0 * u.K * u.km / u.s,  # type: ignore
+    T_bg: u.K = 2.73 * u.K,  # type: ignore
+) -> u.cm**-2:  # type: ignore
     """
     Column density determination for the para-H2CO (1_{11}-1{01}) transition.
     The frequency and Einstein coefficient are obtained from LAMBDA database.
@@ -217,24 +234,25 @@ def o_H2CO_thin(
         The integrated intensity.
     T_bg : u.K
         The background temperature.
-    
+
     Returns
     -------
     Ncol : u.cm**-2
         The column density.
     """
-    trans_index = np.where(o_trans_dict['freq'] == freq.value)[0][0]
-    A_ul = o_trans_dict['A_ul'][trans_index] / u.s
-    upper_level_no = o_trans_dict['upper_level_no'][trans_index] #new
-    upper_level_index = upper_level_no-1 #new
+    trans_index = np.where(o_trans_dict["freq"] == freq.value)[0][0]
+    A_ul = o_trans_dict["A_ul"][trans_index] / u.s  # type: ignore
+    upper_level_no = o_trans_dict["upper_level_no"][trans_index]  # new
+    upper_level_index = upper_level_no - 1  # new
     Jex = J_nu(Tex=Tex, freq=freq)
     Jbg = J_nu(Tex=T_bg, freq=freq)
     Ncol = (
         (8 * np.pi * freq**3 / c**3)
         * Q_o_H2CO(Tex=Tex)
         / A_ul
-        / Q_o_H2CO_i(upper_level_index, Tex=Tex) #modified
+        / Q_o_H2CO_i(upper_level_index, Tex=Tex)  # modified
         / (np.exp(h * freq / k_B / Tex) - 1)
-        * TdV/ (Jex - Jbg)
+        * TdV
+        / (Jex - Jbg)
     )
     return Ncol
